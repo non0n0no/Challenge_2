b542919aabd652bb81d5cbe44f0b6f4dacd4f36b
non0n0no <a55120@alunos.ipb.pt>
2024-06-30 17:58:38 +0100

    Final_Commit

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -6,13 +6,22 @@ public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
 
+    float dogTimer;
+
+    [SerializeField] private float dogColdown = 1f;
+
     // Update is called once per frame
     void Update()
     {
+
+        dogTimer += Time.deltaTime;
+
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) &&  dogTimer > dogColdown)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+            dogTimer = 0f;
         }
     }
 }
--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -25,8 +25,13 @@ public class SpawnManagerX : MonoBehaviour
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
+        int randomNumber = Random.Range(0, ballPrefabs.Length);
+
+        //interval with time oscillation
+        spawnInterval = Random.Range(3, 5);
+
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[randomNumber], spawnPos, ballPrefabs[randomNumber].transform.rotation);
     }
 
 }
